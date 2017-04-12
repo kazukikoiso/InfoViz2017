02f01f73372f835791d6e29428536e38bf277e3e
小磯　一貴 <1465033t@istc-a-064.epc.center.kobe-u.ac.jp>
2017-04-12 16:34:43 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -5,6 +5,22 @@ vec3 = function(x,y,z)
     this.z=z;
 }
 
+vec3.prototype.add = function(v)
+{
+    this.x += v.x;
+    this.y += v.y;
+    this.z += v.z;
+    return this;
+}
+
+vec3.prototype.sub = function(v)
+{
+    this.x -= v.x;
+    this.y -= v.y;
+    this.z -= v.z;
+    return this;
+}
+
 vec3.prototype.max = function(v)
 {
     var x = v.x;
@@ -41,4 +57,29 @@ vec3.prototype.min = function(v)
 vec3.prototype.mid = function(v)
 {
     return this.x + this.y + this.z - this.min(v) - this.max(v);
+}
+
+vec3.prototype.crossproduct = function(v)
+{
+            var x = this.x;
+        var y = this.y;
+        var z = this.z;
+
+        this.x = y * v.z - z * v.y;
+        this.y = z * v.x - x * v.z;
+        this.z = x * v.y - y * v.x;
+
+        return this;
+}
+
+vec3.prototype.length = function(v)
+{
+    return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
+}
+
+function AreaOfTriangle(v0,v1,v2)
+{
+    var v10 = v0.sub(v1);
+    var v12 = v2.sub(v1);
+    return 0.5*v10.crossproduct(v12).length();   
 }
